294795dad67952ddba15da575cff6e9dba1cc3f8
dmx <vincent.f@intp.com>
2024-04-11 10:18:51 +0400

    (add) website link in header

--- src/src/layout/Header.vue
+++ src/src/layout/Header.vue
@@ -11,6 +11,13 @@
       </div>
     </div>
     <div class="ml-auto flex space-x-3">
+      <a href="https://sdfx.ai" target="_blank" class="flex items-center group font-semibold text-zinc-500 hover:text-teal-800 dark:text-zinc-400 dark:hover:text-teal-200 rounded-md border border-zinc-300 hover:border-teal-600 dark:border-zinc-800 dark:hover:border-teal-700 text-xs space-x-2 px-2 h-8 justify-center">
+        <svg class="w-4 h-4 text-zinc-400 dark:text-zinc-600 group-hover:text-teal-500 dark:group-hover:text-teal-400" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"></path>
+        </svg>
+        <span>Website</span>
+      </a>
+
       <a href="https://t.me/sdfxai" target="_blank" class="flex items-center group font-semibold text-zinc-500 hover:text-teal-800 dark:text-zinc-400 dark:hover:text-teal-200 rounded-md border border-zinc-300 hover:border-teal-600 dark:border-zinc-800 dark:hover:border-teal-700 text-xs space-x-2 px-2 h-8 justify-center">
         <svg class="w-4 h-4 text-zinc-400 dark:text-zinc-600 group-hover:text-teal-500 dark:group-hover:text-teal-400" fill="currentColor" viewBox="0 0 512 512"><path d="M477 43.86L13.32 223.29a5.86 5.86 0 0 0-.8.38c-3.76 2.13-30 18.18 7 32.57l.38.14 110.41 35.67a6.08 6.08 0 0 0 5.09-.62l273.85-170.86a6 6 0 0 1 2.2-.83c3.81-.63 14.78-1.81 7.84 7-7.85 10-194.9 177.62-215.66 196.21a6.3 6.3 0 0 0-2.07 4.17l-9.06 108a7.08 7.08 0 0 0 2.83 5.67 6.88 6.88 0 0 0 8.17-.62l65.6-58.63a6.09 6.09 0 0 1 7.63-.39l114.45 83.1.37.25c2.77 1.71 32.69 19.12 41.33-19.76l79-375.65c.11-1.19 1.18-14.27-8.17-22-9.82-8.08-23.72-4-25.81-3.56a6 6 0 0 0-.9.33z"/></svg>
         <span>Telegram</span>
