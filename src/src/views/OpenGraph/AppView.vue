dc1921111e1fcccd061ea4919dc4c8a71516d368
dmx <vincent.f@intp.com>
2024-06-29 23:46:33 +0400

    (update) empty app message

--- src/src/views/OpenGraph/AppView.vue
+++ src/src/views/OpenGraph/AppView.vue
@@ -8,7 +8,7 @@
       <svg class="w-24 h-24 text-zinc-300 dark:text-zinc-700" fill="none" stroke="currentColor" stroke-width="1.0" viewBox="0 0 24 24" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"></path>
       </svg>
-      <span class="max-w-xs text-lg text-center mt-3">Please drag'n drop a SDFX app, an image or a Comfy Workflow.</span>
+      <span class="max-w-xs text-lg text-center mt-3">Please drag'n drop a SDFX app or load one from the left menu.</span>
     </div>
 
     <!-- has workflow but no mainpane mapping -->
